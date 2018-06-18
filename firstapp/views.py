290ff8f6806968340036d1e7589e1b49dce1d427
Ritwick Verma <verma1997@gmail.com>
2018-06-19 00:23:37 +0530

    accessing all data

--- firstapp/views.py
+++ firstapp/views.py
@@ -16,7 +16,7 @@ def index(request):
         for i in range(len(lis)):
             ws.cell(row=r,column=i+1).value=lis[i]
 
-    fieldnames=['name','description','template name','object_id','type','db_field','data_type','label','required','active','disabled']
+    fieldnames=['NAME','DESCRIPTION','TEMPLATE NAME','OBJECT_ID','TYPE','DB_FIELD','DATA_TYPE','LABEL','REQUIRED','ACTIVE','DISABLED']
     printtoexcel(fieldnames,1)
 
     r=2
@@ -27,7 +27,7 @@ def index(request):
             l2=l1+[temp.name,temp.object_id,temp.type]
             conftemps=ConfigurableTemplate.objects.filter(template__name=temp.name)
             for conftemp in conftemps:
-                l3=l2+[conftemp.db_field,conftemp.data_type,conftemp.label,conftemp.required,conftemp.active,conftemp.disabled]
+                l3=l2+[conftemp.db_field,conftemp.data_type,conftemp.label,'True' if conftemp.required else 'False','True' if conftemp.active else 'False','True' if conftemp.disabled else 'False']
                 printtoexcel(l3,r)
                 r+=1
 
